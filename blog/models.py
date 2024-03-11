1df7f01f7acb5921de90dd816f50dfdff50570d2
Lower <loweroficial1@gmail.com>
2024-03-11 14:14:15 -0300

    fist commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+# Create your models here.
+class post(models.Model):
+    title =  models.CharField(max_length=255)
+    summary = RichTextField()
+    content = RichTextUploadingField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
